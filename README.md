530edcacf311ab89f50112e0e5ecb163f506a089
Neil Raina <mrneilraina@gmail.com>
2023-09-30 16:14:01 -0400

    chore: setup custom app base repo

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+This is a base Copilot Custom app built using [Next.js](https://nextjs.org/) and project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
 
-First, run the development server:
+The easiest way to get started is to fork this repo. Once forked, you will need to deploy the app andd it to Copilot.
+
+### Deploying
+
+The easiest way to deploy this custom app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme).
+
+- Create a new project in your Vercel account. Note: create an account if you don't have one using github to automatically import repos.
+- Select the forked repo in Import Git Repository
+- Set the COPILOT_API_KEY environment variable in your Vercel project settings. You can find your API key in Copilot under Settings > API. Note: You will need to be an admin to create an API Key.
+
+### Connecting with Copilot
+
+Use the deployed url on Vercel and [create a custom app](https://www.copilot.com/guide/other-apps#don't-see-your-app-h3) in Copilot. You will now be able to use the app in Copilot.
+
+## Developing App
+
+Clone your forked app locally and run the development server.
 
 ```bash
 npm run dev
@@ -17,20 +33,3 @@ bun dev
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
 You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,49 @@
+import { Client, Company, CopilotAPI } from '@/utils/copilotApiUtils'
 import Image from 'next/image'
 
-export default function Home() {
+type SearchParams = { [key: string]: string | string[] | undefined }
+
+async function getContent(searchParams: SearchParams) {
+  if (!process.env.COPILOT_API_KEY) {
+    throw new Error('Missing COPILOT_API_KEY')
+  }
+
+  const copilotAPI = new CopilotAPI(process.env.COPILOT_API_KEY)
+  const result: { client?: Client, company?: Company } = {};
+
+  if (searchParams.clientId && typeof searchParams.clientId === 'string') {
+    result.client = await copilotAPI.getClient(searchParams.clientId)
+  }
+
+  if (searchParams.companyId && typeof searchParams.companyId === 'string') {
+    result.company = await copilotAPI.getCompany(searchParams.companyId)
+  }
+  
+  return result;
+}
+
+export default async function Page({ searchParams }:  { searchParams: SearchParams }) {
+  const data = await getContent(searchParams)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+          Welcome to the custom app&nbsp;
+          <code className="font-mono font-bold">{data.client ? data.client.givenName : data.company?.name}</code>,
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://copilot.com"
             target="_blank"
             rel="noopener noreferrer"
           >
             By{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/copilot_icon.png"
+              alt="Copilot Icon"
               className="dark:invert"
-              width={100}
+              width={24}
               height={24}
               priority
             />
@@ -31,17 +54,17 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src="/copilot_logo.png"
+          alt="Copilot Logo"
+          width={200}
+          height={50}
           priority
         />
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://docs.copilot.com/reference/introduction"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -53,12 +76,12 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            Find detailed information about the Copilot API.
           </p>
         </a>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://docs.copilot.com/page/custom-apps"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -70,29 +93,29 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Learn how to integrate a custom app into the Copilot platform!
           </p>
         </a>
 
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.notion.so/copilotplatforms/Sept-2023-Engaging-Agencies-for-custom-apps-8eb2f8dbdfb5422ca17068e0628c6fb0?pvs=4"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
+            Explore{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
+            Explore the ideas for custom apps.
           </p>
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://.../repo"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -104,7 +127,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Read the details in the repo docs to learn how to deploy.
           </p>
         </a>
       </div>
