8a0956306117440a883c8cbad902f79738aff216
Kevin Foley <kevin.james.foley@gmail.com>
2024-03-19 22:46:48 -0400

    chore: add error page, improve copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,16 +56,11 @@ async function Content({ searchParams }: { searchParams: SearchParams }) {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Welcome to the custom app
-          {data.client && (
-            <>
-              &nbsp;
-              <code className="font-mono font-bold">
-                {data.client ? data.client.givenName : data.company?.name}
-              </code>
-              ,
-            </>
-          )}
+          Welcome to the custom app&nbsp;
+          <code className="font-mono font-bold">
+            {data.client ? data.client.givenName : data.company?.name}
+          </code>
+          ,
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -98,7 +93,7 @@ async function Content({ searchParams }: { searchParams: SearchParams }) {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
         <a
           href="https://docs.copilot.com/reference/introduction"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -117,7 +112,7 @@ async function Content({ searchParams }: { searchParams: SearchParams }) {
         </a>
 
         <a
-          href="https://docs.copilot.com/page/custom-apps"
+          href="https://docs.copilot.com/docs/custom-apps-developing-your-first-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -129,7 +124,7 @@ async function Content({ searchParams }: { searchParams: SearchParams }) {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn how to integrate a custom app into the Copilot platform!
+            Learn how to integrate a custom app into the Copilot platform.
           </p>
         </a>
 
@@ -147,23 +142,6 @@ async function Content({ searchParams }: { searchParams: SearchParams }) {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Explore the ideas for custom apps.
-          </p>
-        </a>
-
-        <a
-          href="https://github.com/copilot-platforms/custom-app-base"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Read the details in the repo docs to learn how to deploy.
           </p>
         </a>
       </div>
