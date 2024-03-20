07dbf147d4b9e6dd1c242b9c1dfe496fc390a4f8
Kevin Foley <kevin.james.foley@gmail.com>
2024-03-19 22:28:32 -0400

    chore: add error page

--- src/components/TokenGate.tsx
+++ src/components/TokenGate.tsx
@@ -1,7 +1,3 @@
-interface PropsBase {
-  searchParams: SearchParams;
-}
-
 export function TokenGate({
   children,
   searchParams,
@@ -10,13 +6,8 @@ export function TokenGate({
   searchParams: SearchParams;
 }) {
   if (!searchParams.token && process.env.COPILOT_ENV !== 'local') {
-    return (
-      <main className="flex flex-col min-h-screen items-center justify-center p-24">
-        <h1 className="text-8xl font-semibold text-center">401</h1>
-        <p className="text-2xl font-semibold text-center">
-          Please provide a token to access the content.
-        </p>
-      </main>
+    throw new Error(
+      'This app requires a token to be passed in the URL. Please check the URL and try again.',
     );
   }
 
