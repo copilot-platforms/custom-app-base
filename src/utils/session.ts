eb28d1eb38bc94255b1f1692f612037ae33b69fc
Jon Polikowski <jon@jupiterlabs.co>
2024-08-07 22:36:05 -0700

    more errors:

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -3,16 +3,6 @@ import { need } from '@/utils/need';
 
 import { unstable_noStore as noStore } from "next/cache";
 
-export default function getEnvVariable(name: string) {
-    // Using noStore prevents this function from being rendered statically
-    // This is a problem because the environment variables need to change between development and production
-    noStore();
-    const variable = process.env[name];
-    if (!variable) {
-        throw new Error("Missing environment variable for " + name);
-    }
-    return variable;
-}
 
 /**
  * A helper function that instantiates the Copilot SDK and fetches data
