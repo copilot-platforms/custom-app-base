93ac8f7738309449cc2f645c341df782d9dc5f05
Allene Norton <norton.allene@gmail.com>
2024-09-18 14:02:03 -0500

    add postMessage

--- src/components/EmbeddedDevOrchestrator.tsx
+++ src/components/EmbeddedDevOrchestrator.tsx
@@ -14,14 +14,43 @@ const EmbeddedDevOrchestrator: React.FC = () => {
     return null;
   };
 
+  let ngrokUrl: string | null = ''
+
+  const [devEnabled, setDevEnabled] = useState(false);
+
   useEffect(() => {
-    const ngrokUrl = getCookie('ngrokUrl');
+    ngrokUrl = getCookie('ngrokUrl');
     console.log({ ngrokUrl });
   }, []);
 
+  const handleClick = () => {
+    if (devEnabled === false) {
+      setDevEnabled(true);
+      console.log(`dev mode enabled`);
+      window.parent.postMessage({
+        enabled: true,
+        type: 'dev.mode',
+        url: ngrokUrl,
+      });
+    } else {
+      setDevEnabled(false);
+      console.log('dev mode disabled');
+      window.parent.postMessage({
+        enabled: false,
+        type: 'dev.mode',
+        url: ngrokUrl,
+      });
+    }
+  };
+
   return (
     <div>
-      <button className="border-2 border-black rounded-md px-4 py-2 fixed bottom-4 right-4">I am a button</button>
+      <button
+        className="border-2 border-black rounded-md px-4 py-2 fixed bottom-4 right-4"
+        onClick={handleClick}
+      >
+        Dev Mode
+      </button>
     </div>
   );
 };
