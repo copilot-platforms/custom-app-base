09be3464a060f587322cd7ada6fa555b46ac8ffa
Kevin Foley <kevin.james.foley@gmail.com>
2025-01-07 10:13:03 -0500

    chore: add support for actions menu and secondary cta

--- src/app/bridge/page.tsx
+++ src/app/bridge/page.tsx
@@ -16,26 +16,6 @@ export default async function Page({
   const workspace = await copilot.retrieveWorkspace();
   return (
     <Container className="max-w-screen-lg">
-      <div className="max-w-prose">
-        <Icon icon="Code" className="w-8 mb-4" />
-
-        <Heading variant="2xl" className="!mb-2">
-          Copilot App Bridge Demo
-        </Heading>
-
-        <Body variant="lg" tag="p" className="!mb-2 text-gray-500">
-          Helper functions for integrating your custom app with your Copilot
-          workspace. Our goal is to enable developers to easily make apps that
-          look and feel like a part of our software, creating a seamless
-          experience for your clients.
-        </Body>
-
-        <Body variant="lg" tag="p" className="!mb-2 text-gray-500">
-          Try it out below, and watch the header of the Copilot dashboard to see
-          what's changing.
-        </Body>
-      </div>
-
       <Demo portalUrl={workspace.portalUrl} />
     </Container>
   );
--- src/bridge/header.ts
+++ src/bridge/header.ts
@@ -2,26 +2,47 @@
 
 import { useEffect, useMemo } from 'react';
 
+type Icons = 'Archive' | 'Plus' | 'Templates' | 'Trash';
+
 interface BreadcrumbsPayload {
-  type: 'header.breadcrumbs';
   items: {
     label: string;
     onClick: string;
   }[];
+  type: 'header.breadcrumbs';
 }
 
 interface PrimaryCtaPayload {
-  type: 'header.primaryCta';
+  icon?: Icons;
   label: string;
-  onClick: string;
+  onClick?: string;
+  type: 'header.primaryCta';
 }
 
-interface Breadcrumb {
+interface SecondaryCtaPayload {
+  icon?: Icons;
+  label: string;
+  onClick?: string;
+  type: 'header.secondaryCta';
+}
+
+interface ActionsMenuPayload {
+  items: {
+    label: string;
+    onClick: string;
+    icon?: Icons;
+  }[];
+  type: 'header.actionsMenu';
+}
+
+interface Clickable {
   label: string;
   onClick?: () => void;
+  icon?: Icons;
 }
 
 const getBreadcrumbId = (idx: number) => `header.breadcrumbs.${idx}`;
+const getActionMenuItemId = (idx: number) => `header.actionsMenu.${idx}`;
 
 const ensureHttps = (url: string) => {
   if (url.startsWith('https://')) {
@@ -34,7 +55,7 @@ const ensureHttps = (url: string) => {
 };
 
 export function useBreadcrumbs(
-  breadcrumbs: Breadcrumb[],
+  breadcrumbs: Clickable[],
   config?: {
     portalUrl?: string;
   },
@@ -95,16 +116,17 @@ export function useBreadcrumbs(
 }
 
 export function usePrimaryCta(
-  primaryCta: Breadcrumb | null,
+  primaryCta: Clickable | null,
   config?: { portalUrl?: string },
 ) {
   const payload: PrimaryCtaPayload | Pick<PrimaryCtaPayload, 'type'> =
     !primaryCta
       ? { type: 'header.primaryCta' }
       : {
-          type: 'header.primaryCta',
+          icon: primaryCta.icon,
           label: primaryCta.label,
           onClick: 'header.primaryCta.onClick',
+          type: 'header.primaryCta',
         };
 
   useEffect(() => {
@@ -142,6 +164,124 @@ export function usePrimaryCta(
           ensureHttps(config.portalUrl),
         );
       }
+    };
+    addEventListener('beforeunload', handleUnload);
+    return () => {
+      removeEventListener('beforeunload', handleUnload);
+    };
+  }, []);
+}
+
+export function useSecondaryCta(
+  secondaryCta: Clickable | null,
+  config?: { portalUrl?: string },
+) {
+  const payload: SecondaryCtaPayload | Pick<SecondaryCtaPayload, 'type'> =
+    !secondaryCta
+      ? { type: 'header.secondaryCta' }
+      : {
+          type: 'header.secondaryCta',
+          label: secondaryCta.label,
+          onClick: 'header.secondaryCta.onClick',
+        };
+
+  useEffect(() => {
+    window.parent.postMessage(payload, 'https://dashboard.copilot.com');
+    if (config?.portalUrl) {
+      window.parent.postMessage(payload, ensureHttps(config.portalUrl));
+    }
+
+    const handleMessage = (event: MessageEvent) => {
+      if (
+        event.data.type === 'header.secondaryCta.onClick' &&
+        typeof event.data.id === 'string' &&
+        secondaryCta?.onClick
+      ) {
+        secondaryCta.onClick();
+      }
+    };
+
+    addEventListener('message', handleMessage);
+
+    return () => {
+      removeEventListener('message', handleMessage);
+    };
+  }, [secondaryCta]);
+
+  useEffect(() => {
+    const handleUnload = () => {
+      window.parent.postMessage(
+        { type: 'header.secondaryCta' },
+        'https://dashboard.copilot.com',
+      );
+      if (config?.portalUrl) {
+        window.parent.postMessage(
+          { type: 'header.secondaryCta' },
+          ensureHttps(config.portalUrl),
+        );
+      }
+    };
+    addEventListener('beforeunload', handleUnload);
+    return () => {
+      removeEventListener('beforeunload', handleUnload);
+    };
+  }, []);
+}
+
+export function useActionsMenu(
+  actions: Clickable[],
+  config?: {
+    portalUrl?: string;
+  },
+) {
+  const callbackRefs = useMemo(() => {
+    return actions.reduce<Record<string, () => void>>(
+      (acc, { onClick }, idx) => {
+        if (onClick) acc[getActionMenuItemId(idx)] = onClick;
+        return acc;
+      },
+      {},
+    );
+  }, [actions]);
+
+  const payload: ActionsMenuPayload = {
+    type: 'header.actionsMenu',
+    items: actions.map(({ label, onClick, icon }, idx) => ({
+      onClick: onClick ? getActionMenuItemId(idx) : '',
+      label,
+      icon,
+    })),
+  };
+
+  useEffect(() => {
+    window.parent.postMessage(payload, 'https://dashboard.copilot.com');
+    if (config?.portalUrl) {
+      window.parent.postMessage(payload, ensureHttps(config.portalUrl));
+    }
+
+    const handleMessage = (event: MessageEvent) => {
+      if (
+        event.data.type === 'header.actionsMenu.onClick' &&
+        typeof event.data.id === 'string' &&
+        callbackRefs[event.data.id]
+      ) {
+        callbackRefs[event.data.id]();
+      }
+    };
+
+    addEventListener('message', handleMessage);
+
+    return () => {
+      removeEventListener('message', handleMessage);
+    };
+  }, [actions, payload]);
+
+  useEffect(() => {
+    const handleUnload = () => {
+      window.parent.postMessage(
+        { type: 'header.actionsMenu', items: [] },
+        'https://dashboard.copilot.com',
+      );
     };
     addEventListener('beforeunload', handleUnload);
     return () => {
